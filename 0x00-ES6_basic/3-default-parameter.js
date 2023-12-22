9be369d8dae5c336f5b86a9223cc1eea0a8b4f38
Trisraki <trisraquel1@gmail.com>
2023-12-22 13:33:20 -0800

    ES6_basic

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+// const getSumOfHoods = (initialNumber,expansion1989 = 89,expansion2019 = 19) => {
+//     return initialNumber + expansion1989 + expansion2019;
+// }
+
+// getSumOfHoods;
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
